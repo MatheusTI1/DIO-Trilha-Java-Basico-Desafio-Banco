1faecd60210ba111af3abd845b61a4c957baabec
MatheusTI1 <matheussouzati1@gmail.com>
2023-05-19 19:53:30 -0300

    Resolução completa do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -11,7 +11,7 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o número da agência !");
         String numeroAgencia = scanner.next();  
-        scanner.nextLine(); // Consumindo linha
+        scanner.nextLine(); // Este foi o meu maior desafio no projeto atual, ao executar este comando "scanner.nextLine()" o interpretador pulava a linha e não permitia a entrada de dados do usuário, utilizar o "scanner.next()" não permitiria que o usuario entrasse com seu nome composto exemplo : "MARIO ANDRADE", ao pesquisar me deparei com o seguinte artigo "https://pt.stackoverflow.com/questions/507845/uso-do-nextline-em-java" e consegui resolver o problema em questão.
 
         System.out.println("Por favor, digite seu nome !");
         String nomeCliente = scanner.nextLine();
